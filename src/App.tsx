5ce75e129bf8e7f3466eac0f615b66894011fff6
Sadra Rahbar <48208938+sadrarahbar@users.noreply.github.com>
2024-06-01 01:23:53 +0330

    create app with vite and add bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css';
+import ListGroup from './components/ListGroup';
+
+function App() {
+
+  return <div><ListGroup></ListGroup></div>;
+}
+
+export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,24 @@
+function ListGroup() {
+  let items = ['tehran', 'saveh', 'hamedan', 'shiraz'];
+    items = [];
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No item found</p> : null;
+  //   };
+  return (
+    <>
+      <h1>List</h1>
+      {/* {getMessage()} */}
+      {/* {items.length === 0 ? <p>No item found</p> : null} */}
+      {items.length === 0 && <p>No item found</p>} {/* trusy */}
+      <ul className='list-group'>
+        {items.map((item, index) => (
+          <li key={index} className='list-group-item'>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
