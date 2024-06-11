03402d4b9314ff87fa8a1e7fc06cff2fb19bfa6a
Sadra Rahbar <48208938+sadrarahbar@users.noreply.github.com>
2024-06-11 07:30:12 +0330

    Add Alert and Button componnents

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
 import './App.css';
-import ListGroup from './components/ListGroup';
+import Alert from './components/Alert';
+import Button from './components/Button';
+// import ListGroup from './components/ListGroup';
 
 function App() {
-  let items = ['tehran', 'saveh', 'hamedan', 'shiraz'];
+  // let items = ['tehran', 'saveh', 'hamedan', 'shiraz'];
+
+  const onClick = () => {
+    console.log('clicked');
+  };
 
   return (
+    // <div>
+    //   <ListGroup items={items} heading='cties'></ListGroup>
+    //   <ListGroup items={items} heading='contries'></ListGroup>
+    // </div>
+    // <div>
+    //   <Alert>
+    //     Hello <h1>World</h1>
+    //   </Alert>
+    // </div>
     <div>
-      <ListGroup items={items} heading='cties'></ListGroup>
-      <ListGroup items={items} heading='contries'></ListGroup>
+      <Button color='secondary' onClick={onClick}>
+        submit
+      </Button>
+      <Button color='warning' onClick={onClick}>
+        cancle
+      </Button>
     </div>
   );
 }
